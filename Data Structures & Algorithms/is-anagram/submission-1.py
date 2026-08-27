36c94779f0f3319ba7eb6d13367fe72ebbf8e593
Matthias Bogarin <mabcsoftwaredev@gmail.com>
2026-08-27 00:23:47 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # Check length condition first, if not the same then return false
+        if len(s) != len(t):
+            return False
+
+        # Convert strings to hash tables with char as key,
+        # value is the count of characters
+        d1, d2 = {}, {}
+        for c in s:
+            d1[c] = d1.get(c, 0) + 1
+        for c in t:
+            d2[c] = d2.get(c, 0) + 1
+
+        # Check if each character has the same count if yes then
+        # return true else return false as soon as you find character
+        # that doesnt have the same count
+        return d1 == d2
